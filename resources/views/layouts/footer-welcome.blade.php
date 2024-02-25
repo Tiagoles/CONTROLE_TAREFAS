0ae5af5f081c48494deaaf935b83d01a31e593ae
Tiago Damasceno <tiagodamascenogames@gmail.com>
2024-02-25 18:40:05 -0300

    corrigindo data footer pagina welcome

--- resources/views/layouts/footer-welcome.blade.php
+++ resources/views/layouts/footer-welcome.blade.php
@@ -1,5 +1,5 @@
 <footer>
     <div class="footer-content">
-        ;&copy; Controle de tarefas. Todos os direitos reservados. 2024
+        ;&copy;Controle de tarefas, Todos os direitos reservados. 2024
     </div>
 </footer>
